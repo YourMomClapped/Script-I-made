1985069f9d25f0b7e97055a956b9b5718cac2e1d
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-13 19:56:36 +0930

    Update .lua

--- .lua
+++ .lua
@@ -6,18 +6,17 @@ local Window = Library.CreateLib("For me", "DarkTheme")
 -- INFINITE YIELD AND SILENT AIM --
 local Main = Window:NewTab("Main")
 -- Infinite Yield --
-local MainSection = Main:NewSection("Infinite Yield")
-MainSection:NewButton("ButtonText", "ButtonInfo", function()
+local MainSection = Main:NewSection("Main")
+MainSection:NewButton("Infinite Yield", "FE admin commands", function()
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
 end)
 -- Silent Aim --
-local MainSection = Main:NewSection("Silent Aim")
 MainSection:NewButton("Universial", "Silent Aim", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Averiias/Universal-SilentAim/main/main.lua"))()
 end)
 -- Chat Spoofer --
 local MainSection = Main:NewSection("Silent Aim")
-MainSection:NewButton("Universial", "Silent Aim", function()
+MainSection:NewButton("Chat Spooofer", "Spoofs your chats.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/Chat-Spoofer/main/.Main"))()
 end)
 
@@ -34,10 +33,15 @@ end)
 
 -- PrisonLife Admin --
 local PrisonlifeSection = Prisonlife:NewSection("PrisonLife Admin")
-PrisonlifeSection:NewButton("ButtonText", "ButtonInfo", function()
+PrisonlifeSection:NewButton("Prison life admin", "FE Prisonlife Admin", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/Prison-Life-Admin/main/.Main"))()
 end)
 end
+-- Other --
+local Other = Window:NewTab("Other")
+local OtherSection = Other:NewSection("Random Stuff")
+
+
 
 
 
